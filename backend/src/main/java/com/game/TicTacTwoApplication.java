fd4c14c5c38aab13e3b0363d26ec8814a747bfe5
athrvk <atharvakusumbia@gmail.com>
2024-12-15 19:15:55 +0530

    Exclude DataSource and Thymeleaf auto-configuration in main application class and update logging settings in production properties

--- backend/src/main/java/com/game/TicTacTwoApplication.java
+++ backend/src/main/java/com/game/TicTacTwoApplication.java
@@ -2,8 +2,13 @@ package com.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        ThymeleafAutoConfiguration.class,
+})
 public class TicTacTwoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicTacTwoApplication.class, args);
